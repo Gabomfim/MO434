b2a0b3ec66592a7649085b4e175a53618b039d72
Gabomfim <gabomfim99@gmail.com>
2026-06-04 18:32:13 -0300

    Set quad ratio to 3 and enable W&B in example scripts

--- RKD/examples/cars196.sh
+++ RKD/examples/cars196.sh
@@ -1,28 +1,39 @@
 #!/usr/bin/env bash
+
+WANDB_PROJECT="rkd-metric-learning"
+WANDB_MODE="online"
+WANDB_GROUP_TEACHER="cars196-teacher"
+WANDB_GROUP_DISTILL="cars196-distill"
+
 # Teacher Network
 python run.py --dataset cars196 --epochs 40 --lr_decay_epochs 25 30 35 --lr_decay_gamma 0.5 --batch 128\
-              --base resnet50 --sample distance --margin 0.2 --embedding_size 512 --save_dir cars196_resnet50_512
+              --base resnet50 --sample distance --margin 0.2 --embedding_size 512 --save_dir cars196_resnet50_512 \
+              --wandb_project "$WANDB_PROJECT" --wandb_mode "$WANDB_MODE" --wandb_group "$WANDB_GROUP_TEACHER" --wandb_run_name cars196_teacher_resnet50_512
 
 # Student with small embedding
 python run_distill.py --dataset cars196 --epochs 80 --lr_decay_epochs 40 60 --lr_decay_gamma 0.1 --batch 128\
                       --base resnet18 --embedding_size 128 --l2normalize false --dist_ratio 1 --angle_ratio 2 \
                       --teacher_base resnet50 --teacher_embedding_size 512 --teacher_load cars196_resnet50_512/best.pth \
-                      --save_dir cars196_student_resnet18_128
+                      --save_dir cars196_student_resnet18_128 --wandb_project "$WANDB_PROJECT" --wandb_mode "$WANDB_MODE" \
+                      --wandb_group "$WANDB_GROUP_DISTILL" --wandb_run_name cars196_student_resnet18_128
 
 # Student with small embedding (Quadruplet-only)
 python run_distill.py --dataset cars196 --epochs 80 --lr_decay_epochs 40 60 --lr_decay_gamma 0.1 --batch 128\
-                      --base resnet18 --embedding_size 128 --l2normalize false --quad_ratio 1 \
+                      --base resnet18 --embedding_size 128 --l2normalize false --quad_ratio 3 \
                       --teacher_base resnet50 --teacher_embedding_size 512 --teacher_load cars196_resnet50_512/best.pth \
-                      --save_dir cars196_student_resnet18_128_quad
+                      --save_dir cars196_student_resnet18_128_quad --wandb_project "$WANDB_PROJECT" --wandb_mode "$WANDB_MODE" \
+                      --wandb_group "$WANDB_GROUP_DISTILL" --wandb_run_name cars196_student_resnet18_128_quad
 
 # Self-Distillation
 python run_distill.py --dataset cars196 --epochs 80 --lr_decay_epochs 40 60 --lr_decay_gamma 0.1 --batch 128\
                       --base resnet50 --embedding_size 512 --l2normalize false --dist_ratio 1 --angle_ratio 2 \
                       --teacher_base resnet50 --teacher_embedding_size 512 --teacher_load cars196_resnet50_512/best.pth \
-                      --save_dir cars196_student_resnet50_512
+                      --save_dir cars196_student_resnet50_512 --wandb_project "$WANDB_PROJECT" --wandb_mode "$WANDB_MODE" \
+                      --wandb_group "$WANDB_GROUP_DISTILL" --wandb_run_name cars196_student_resnet50_512
 
 # Self-Distillation (Quadruplet-only)
 python run_distill.py --dataset cars196 --epochs 80 --lr_decay_epochs 40 60 --lr_decay_gamma 0.1 --batch 128\
-                      --base resnet50 --embedding_size 512 --l2normalize false --quad_ratio 1 \
+                      --base resnet50 --embedding_size 512 --l2normalize false --quad_ratio 3 \
                       --teacher_base resnet50 --teacher_embedding_size 512 --teacher_load cars196_resnet50_512/best.pth \
-                      --save_dir cars196_student_resnet50_512_quad
+                      --save_dir cars196_student_resnet50_512_quad --wandb_project "$WANDB_PROJECT" --wandb_mode "$WANDB_MODE" \
+                      --wandb_group "$WANDB_GROUP_DISTILL" --wandb_run_name cars196_student_resnet50_512_quad
